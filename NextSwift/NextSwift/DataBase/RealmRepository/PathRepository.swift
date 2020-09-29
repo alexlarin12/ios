a8200db0ce83e10c4c960dd13a242cbc28a1671a
alexlarin <alex12081973@mail.ru>
2020-09-29 19:24:33 +0300

    lesson2

---
+++ NextSwift/NextSwift/DataBase/RealmRepository/PathRepository.swift
@@ -0,0 +1,23 @@
+//
+//  PathRepository.swift
+//  NextSwift
+//
+//  Created by Alex Larin on 29.09.2020.
+//
+
+import Foundation
+import RealmSwift
+
+class PathRepository{
+    
+  // метод сохранения координат точки в Realm:
+    func savePathData(){
+        
+    }
+    
+    // метод получения пользователя из Realm:
+    func getPathData(){
+       
+    }
+    
+}
